f7de84b344c93a23695a167f59f7577f79ffc8a9
Gordon Grabert <gordon.grabert@tu-braunschweig.de>
2025-02-27 15:23:52 +0100

    better evd

--- vignettes/Simulation_GRM.R
+++ vignettes/Simulation_GRM.R
@@ -7,6 +7,7 @@ require(ggExtra)
 require(AGHmatrix)
 require(MASS)
 require(mvnfast)
+require(multivarious)
 
 
 #### Use SNP data from AGHmatrix #####
@@ -285,6 +286,128 @@ cov_matrix_E
 
 
 
+mt_evd_2 <- function(n, grm, n_resp, resp.m = NULL, model, n_pc = 20, formula = NULL, data = NULL){
 
+  ####################################################################
+  ## Prepare matrices ################################################
+  ####################################################################
+
+  grm_sparse <- Matrix(grm, sparse = FALSE)
+  A <- as(grm_sparse, "dsCMatrix")
+
+  # Eigen decomposition of GRM
+  evd <- eigen(A, symmetric = TRUE)
+  Q <- evd$vectors
+  lambda <- evd$values
+
+  # Reduce Q
+  n <- length(lambda) - n_pc
+  P <- Q[, 1:n_pc]
+  D <- diag(lambda[1:n_pc])
+
+  # Transform response variable matrix
+  trans.resp.m <- t(P) %*% as.matrix(resp.m)
+  E <- diag(nrow(trans.resp.m))
+
+
+  ####################################################################
+  ## Extending to multivariate responses #############################
+  ####################################################################
+  output <- list()
+
+  if (n_resp > 1) {
+    Z_struc <- mglm4twin:::mt_struc(n_resp = n_resp)
+    ind_A <- lapply(Z_struc, function(x) kronecker(x, D))
+    ind_E <- lapply(Z_struc, function(x) kronecker(x, E))
+  }
+
+  ####################################################################
+  ## Selecting the different twin models #############################
+  ####################################################################
+  if (n_resp > 1) {
+    if (model == "AE") {
+      output$matrices <- c(ind_A, ind_E)
+      output$phenotype <- trans.resp.m
+    }
+  } else {
+    if (model == "E") {
+      output <- list(ind_E)
+    } else if (model == "AE") {
+      output$matrices <- c(ind_A, ind_E)
+      output$phenotype <- trans.resp.m
+    }
+  }
+
+  ####################################################################
+  ## Applying formula-based transformations ##########################
+  ####################################################################
+  if (!is.null(formula)) {
+    if (length(output) != length(formula)) {
+      stop("Error: Number of formulas does not match number of dispersion components")
+    }
+    X_list <- lapply(formula, model.matrix, data = data)
+    list_final <- lapply(seq_along(output), function(i) {
+      lapply(seq_len(ncol(X_list[[i]])), function(j) {
+        X_list[[i]][, j] * output[[i]]
+      })
+    })
+    output <- do.call(c, list_final)
+  }
+
+  return(output)
+}
+
+
+mat <- mt_evd_2(grm = GRM, n_resp = 3, resp.m = as.data.frame(pheno_col), n_pc = 926, model = "AE", data = data)
+
+
+
+# Prepare for mglm4twin
+
+# Fixed effects
+
+linear_pred_1 <- V1 ~ 1
+linear_pred_2 <- V2 ~ 1
+linear_pred_3 <- V3 ~ 1
+
+data <- as.data.frame(mat$phenotype)
+
+res <- mglm4twin(linear_pred = c(linear_pred_1, linear_pred_2, linear_pred_3),
+                 matrix_pred = c(mat$matrices),
+                 data = data)
+
+A11 <- res$Covariance[1]
+A22 <- res$Covariance[2]
+A33 <- res$Covariance[3]
+A21 <- res$Covariance[4]
+A31 <- res$Covariance[5]
+A32 <- res$Covariance[6]
+
+E11 <- res$Covariance[7]
+E22 <- res$Covariance[8]
+E33 <- res$Covariance[9]
+E21 <- res$Covariance[10]
+E31 <- res$Covariance[11]
+E32 <- res$Covariance[12]
+
+
+
+# Construct covariance matrix
+cov_matrix_A <- matrix(c(
+  A11, A21, A31,
+  A21, A22, A32,
+  A31, A32, A33
+), nrow = 3, byrow = TRUE)
+
+cov_matrix_A
+
+# Construct covariance matrix
+cov_matrix_E <- matrix(c(
+  E11, E21, E31,
+  E21, E22, E32,
+  E31, E32, E33
+), nrow = 3, byrow = TRUE)
+
+cov_matrix_E
 
 
